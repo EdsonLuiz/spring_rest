f85c0e361495a86cfc4c51dcc03665a6bc89102f
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-04 13:48:46 -0300

    Get requests return a List of Categorias
    When CategoriaResource receive a get request it response with a List
    of Categorias

--- src/main/java/estudo/cursomc/resources/CategoriaResource.java
+++ src/main/java/estudo/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,14 @@
 package estudo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import estudo.cursomc.domains.Categoria;
 
 /**
  * CategoriaResource
@@ -12,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
     @GetMapping
-    public String show() {
-        return "ok";
+    public List<Categoria> show() {
+
+        List<Categoria> categorias = new ArrayList<>(
+                Arrays.asList(new Categoria(1, "Informática"), new Categoria(2, "Escritório")));
+        return categorias;
     }
 
 }
