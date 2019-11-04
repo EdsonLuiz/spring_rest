5bebf3b6bd01f30ab5795de5bfa9a3e6998e8957
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-04 15:01:27 -0300

    🚧 Add annotations to Categoria

--- src/main/java/estudo/cursomc/domains/Categoria.java
+++ src/main/java/estudo/cursomc/domains/Categoria.java
@@ -2,6 +2,11 @@ package estudo.cursomc.domains;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +19,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
