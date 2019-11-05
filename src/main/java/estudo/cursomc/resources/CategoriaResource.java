6521c27b759f5a4fec270c3f2ca4a06bf20d8074
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-05 14:05:54 -0300

    ♻ Add get categorias/{id} resource

--- src/main/java/estudo/cursomc/resources/CategoriaResource.java
+++ src/main/java/estudo/cursomc/resources/CategoriaResource.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import estudo.cursomc.domains.Categoria;
+import estudo.cursomc.services.CategoriaService;
 
 /**
  * CategoriaResource
@@ -17,12 +20,24 @@ import estudo.cursomc.domains.Categoria;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
+    private CategoriaService categoriaService;
+
+    public CategoriaResource(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
     @GetMapping
     public List<Categoria> show() {
 
         List<Categoria> categorias = new ArrayList<>(
                 Arrays.asList(new Categoria(1, "Informática"), new Categoria(2, "Escritório")));
         return categorias;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> index(@PathVariable Integer id) {
+        Categoria obj = categoriaService.index(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
