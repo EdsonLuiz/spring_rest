e1a69d2ee15d78183890c6c0948912c4bc3d7bc6
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2019-11-05 14:33:28 -0300

    ✨ Populate database on when program run

--- src/main/java/estudo/cursomc/Application.java
+++ src/main/java/estudo/cursomc/Application.java
@@ -1,13 +1,32 @@
 package estudo.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import estudo.cursomc.domains.Categoria;
+import estudo.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+
 	}
 
 }
